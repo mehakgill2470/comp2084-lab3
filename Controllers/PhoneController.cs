a7eef73c94f979a516ddf1e9b2dddb1d691082f5
Mehak Gill <mehakgill2470@gmail.com>
2020-01-23 21:41:10 -0500

    lab3 solution files

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone{PhoneID = 7864533, PhoneName = "A50", Manufacturer = "Samsung", MSRP = 400, ScreenSize = 6 ,DateReleased = new DateTime(2019,07,23)}
+            };
+            return View(phoneList);
+        }
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            DateReleased = DateTime.Now;
+        }
+
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public int MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+
+    }
+}
